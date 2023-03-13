d51e202e17fcc210c4bba45365b2b9a05cb06f1c
NetilogyIvan <inkornelyuk@gmail.com>
2023-03-13 21:33:20 +0600

    kod bonuse added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int score = 2_000;
+        int payment = 500;
+
+        int bonus;
+        if (payment > 1_000) {
+            bonus = payment / 100;
+        } else {
+            bonus = 0;
+        }
+
+        int balance = score + payment + bonus;
+
+        System.out.println("Ваш счет пополнен на " + payment + " руб., Баланс " + balance + " руб., Ваш бонус " + bonus + " руб.,");
     }
 }
